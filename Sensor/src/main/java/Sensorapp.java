7ba7d6183c75e3782811b670c57316a732844b62
shanesoysa <shanesanjaya@gmail.com>
2020-05-07 01:56:34 +0530

    sensor application

---
+++ Sensor/src/main/java/Sensorapp.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import sensor.model.Sensor;
+
+public class Sensorapp {
+
+	public static void main(String[] args) {
+		
+	
+		
+		Sensor sensor=new Sensor();
+		
+		
+			
+		
+		sensor.updateSensor(sensor);
+		
+
+		
+		
+		
+		
+
+		
+	}
+
+}
