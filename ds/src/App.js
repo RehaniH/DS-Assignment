675c0ca14d8bf8da9ff7255ab5e930673e3dcc6c
andrew-peries14 <63182149+andrew-peries14@users.noreply.github.com>
2020-05-07 02:29:11 +0530

    Add files via upload

---
+++ ds/src/App.js
@@ -0,0 +1,16 @@
+//App.js
+import React from 'react';
+
+import './App.css';
+import Details from "./components/Details";
+//calls the Details component
+function App() {
+  return (
+      <div>
+
+          <Details/>
+      </div>
+  );
+}
+
+export default App;
